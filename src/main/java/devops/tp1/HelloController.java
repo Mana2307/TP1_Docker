572283e19531d64046dfb5ff38109ca25fd120db
Anthony Manouana <anthony.manouana@universite-paris-saclay.fr>
2026-03-06 11:12:55 +0100

    Ajout du TP1

---
+++ src/main/java/devops/tp1/HelloController.java
@@ -0,0 +1,13 @@
+package devops.tp1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/sayHello")
+    public String sayHello() {
+        return "Hello";
+    }
+}
